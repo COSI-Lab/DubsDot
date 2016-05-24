1cf377c949bc93db0899ea621efef4a8f5a49c08
Benjamin Lannon <brlannon118@gmail.com>
2016-05-24 14:47:09 -0400

    Cleaning and refactoring some code

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,18 +3,21 @@ var sass = require('gulp-sass');
 var pug = require('gulp-pug');
 var exec = require('child_process').exec;
 
+// Proccess sass to css
 gulp.task('sass', function() {
   return gulp.src("./sass/main.scss")
     .pipe(sass({outputStyle: 'expanded'}))
     .pipe(gulp.dest("./css"));
 });
 
+// Proccess pug to html
 gulp.task('pug', function() {
   return gulp.src("./*.pug")
     .pipe(pug({pretty: true}))
     .pipe(gulp.dest("./"));
 });
 
+// gen links.pug from links.json
 gulp.task('generate', function() {
 	exec("node link-gen.js");
 });
--- link-gen.js
+++ link-gen.js
@@ -1,25 +1,42 @@
 var src = require('./links.json');
 var fs = require('fs');
 
-var result = "";
+let result = dubsdotJsonToPug(src);
 
-for(let item in src) {
-	let name = item;
-	let content = src[item];
+writeToFile("./includes/links.pug", result);
 
-	result += "section\n\th1 " + name + "\n\tul\n";
-	for(let link in content) {
-		let linkObj = content[link];
-		result += "\t\tli\n\t\t\ta(href=\"" + linkObj.url + "\") " + linkObj.name + "\n";
-		result += "\t\t\tp " + linkObj.desc + "\n";
+function dubsdotJsonToPug(src) {
+	let result = "";
+
+	// Loop through each section
+	for(let item in src) {
+		let name = capitalize(item);
+		let content = src[item];
+
+		result += "section\n\th1 " + name + "\n\tul\n";
+
+		// Loop through each link in section
+		for(let link in content) {
+			let linkObj = content[link];
+			result += "\t\tli\n"; // <li>
+			result += "\t\t\ta(href=\"" + linkObj.url + "\") " + linkObj.name + "\n"; // <a>
+			result += "\t\t\tp " + linkObj.desc + "\n"; // <p>
+		}
 	}
+
+	return result;
 }
 
-console.log(result);
-fs.writeFile("./includes/links.pug", result, err => {
-	if(err) {
-		return console.log(err);
-	}
+function writeToFile(filename, str) {
+	fs.writeFile(filename, str, err => {
+		if(err) {
+			return console.log(err);
+		}
 
-	console.log("File saved");
-});
+		console.log("File saved");
+	});
+}
+
+function capitalize(str) {
+	return str[0].toUpperCase() + str.slice(1);
+}
